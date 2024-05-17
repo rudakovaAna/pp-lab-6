7da4e31ce5b1ca22ef0efb95b43ba4607a421730
Ana R <nastarudakova@gmail.com>
2024-05-17 20:14:06 +0200

    zad3

--- company/Main.java
+++ company/Main.java
@@ -15,20 +15,45 @@ public class Main {
         Worker worker4 = new Worker("Eva", 2050.0, 4, "2022-04-01", "Worker");
         Manager manager1 = new Manager("Michael", 3500.0, 5, "2020-05-01", "Manager");
 
+        Worker worker5 = new Worker("David", 2100.0, 2, "2021-06-01", "Worker");
+        Manager manager2 = new Manager("Sarah", 3800.0, 2, "2021-07-01", "Manager");
+
         List<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
         employees.add(worker4);
         employees.add(manager1);
+        employees.add(worker5);
+        employees.add(manager2);
+
+        double totalSalary = 0;
+        double totalManagerSalary = 0;
+        double totalWorkerSalary = 0;
 
         for (Employee employee : employees) {
-            System.out.println("Salary of " + employee.getName() + ": " + employee.getSalary());
-            employee.work();
-            System.out.println("- " + employee.getName() + " (ID: " + employee.getId() + 
-                               ", Position: " + employee.getPosition() + 
-                               ", Hire date: " + employee.getHireDate() + 
-                               ", Salary: " + employee.getSalary() + ")");
+            totalSalary += employee.getSalary();
+            if (employee instanceof Manager) {
+                totalManagerSalary += employee.getSalary();
+            } else if (employee instanceof Worker) {
+                totalWorkerSalary += employee.getSalary();
+            }
+        }
+
+        System.out.println("Total salary of all employees: " + totalSalary);
+        System.out.println("Total salary of all managers: " + totalManagerSalary);
+        System.out.println("Total salary of all workers: " + totalWorkerSalary);
+
+        for (Employee employee : employees) {
+            System.out.println(employee.getName() + " has code: " + employee.hashCode());
+        }
+
+        Worker workerDuplicate = new Worker("David", 2100.0, 2, "2021-06-01", "Worker");
+
+        for (Employee employee : employees) {
+            if (workerDuplicate.equals(employee) && workerDuplicate != employee) {
+                System.out.println("The ID of " + workerDuplicate.getName() + " matches with " + employee.getName());
+            }
         }
     }
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -41,4 +41,13 @@ public abstract class Employee implements Employable {
     public int hashCode() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Employee employee = (Employee) obj;
+        return id == employee.id;
+    }
 }
+
